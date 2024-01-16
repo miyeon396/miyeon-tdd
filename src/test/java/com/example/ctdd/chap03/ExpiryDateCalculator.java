c6f9edbebee54785c8f867f3f9420b36d86e549e
miyeon396 <miyeon396@gmail.com>
2024-01-16 22:41:35 +0900

    03_작성_순서 : 다음 테스트는 쉬운 테스트 및 예외 상황 추가
    - 이만원 이상 케이스 추가
    - 납부일 만료일 다를 떄 이만원 이상

--- src/test/java/com/example/ctdd/chap03/ExpiryDateCalculator.java
+++ src/test/java/com/example/ctdd/chap03/ExpiryDateCalculator.java
@@ -1,15 +1,20 @@
 package com.example.ctdd.chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(PayData payData) {
-        int addedMonth = 1;
+        int addedMonth = payData.getPayAmount() / 10_000;
 
         if (payData.getFirstBillingDate() != null) {
             LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonth);
             if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                if (YearMonth.from(candidateExp).lengthOfMonth() <
+                payData.getFirstBillingDate().getDayOfMonth()) {
+                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth());
+                }
                 return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
             }
         }
