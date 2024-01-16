4217f0f5ad47b8e4164c25710a00b59941fb279b
miyeon396 <miyeon396@gmail.com>
2024-01-16 20:50:29 +0900

    03_작성_순서 : 테스트 코드 작성 후 통과하게 상수 지정 리턴
    - 가장 쉬운코드 부터 작성하라
    - 처음에 가장 쉽게 통과하게 만들어라
    - 클래스도 새로 만들고, 클래스에서는 2019-4-1을 그대로 리턴해서
    - 무조건 통과하게

---
+++ src/test/java/com/example/ctdd/chap03/ExpiryDateCalculator.java
@@ -0,0 +1,10 @@
+package com.example.ctdd.chap03;
+
+import java.time.LocalDate;
+
+public class ExpiryDateCalculator {
+
+    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
+        return LocalDate.of(2019,4,1);
+    }
+}
---
+++ src/test/java/com/example/ctdd/chap03/ExpiryDateCalculatorTest.java
@@ -0,0 +1,31 @@
+package com.example.ctdd.chap03;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ExpiryDateCalculatorTest {
+    /**
+     * 매달 비용을 지불해야 사용 가능 유료서비스. 규칙에 따른 서비스 만료일 결정
+     * 1. 사용하려면 매달 선불 납부 필요 납부일 기준 한달 뒤가 서비스 만료일
+     * 2. 2개월 이상 요금을 납부할 수 있음
+     * 3. 10만원을 납부하면 서비스를 1년 제공
+     * -> 납부 금액 기준 서비스 만료일 계산
+     */
+
+    // 구현 쉬운것 부터 먼저 테스트
+    // 예외 상황부터 먼저 테스트
+
+    @Test
+    void 만원_납부하면_한달_뒤가_만료일이_됨() {
+        LocalDate billingDate = LocalDate.of(2019,3,1);
+        int payAmount = 10_000;
+
+        ExpiryDateCalculator cal = new ExpiryDateCalculator();
+        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
+
+        assertEquals(LocalDate.of(2019,4,1), expiryDate);
+    }
+}
